433449c26b80e4e406ce13f145a87c6526c1ac90
Benoît Legat <benoit.legat@gmail.com>
2017-01-04 11:35:34 +0100

    Use testset and drop Julia v0.4

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -1,14 +1,16 @@
-const mathprogbase_test = joinpath(Pkg.dir("MathProgBase"), "test")
+@testset "MathProgBase tests" begin
+    const mathprogbase_test = joinpath(Pkg.dir("MathProgBase"), "test")
 
-include(joinpath(mathprogbase_test, "linprog.jl"))
-linprogtest(GLPKSolverLP())
+    include(joinpath(mathprogbase_test, "linprog.jl"))
+    linprogtest(GLPKSolverLP())
 
-include(joinpath(mathprogbase_test, "linproginterface.jl"))
-linprogsolvertest(GLPKSolverLP())
+    include(joinpath(mathprogbase_test, "linproginterface.jl"))
+    linprogsolvertest(GLPKSolverLP())
 
-include(joinpath(mathprogbase_test, "mixintprog.jl"))
-mixintprogtest(GLPKSolverMIP())
+    include(joinpath(mathprogbase_test, "mixintprog.jl"))
+    mixintprogtest(GLPKSolverMIP())
 
-include(joinpath(mathprogbase_test,"conicinterface.jl"))
-coniclineartest(GLPKSolverLP())
-coniclineartest(GLPKSolverMIP())
+    include(joinpath(mathprogbase_test,"conicinterface.jl"))
+    coniclineartest(GLPKSolverLP())
+    coniclineartest(GLPKSolverMIP())
+end
