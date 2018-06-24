554f78962fe8095968b5ebc7badbb51d049f2eeb
Robert Schwarz <mail@rschwarz.net>
2018-06-24 21:20:36 +0200

    add more tests

--- test/params.jl
+++ test/params.jl
@@ -6,3 +6,16 @@
     @test mipm.smplxparam.it_lim == 5910
     @test mipm.param.tol_obj == 1.52e-3
 end
+
+@testset "Set MPB parameters" begin
+    # setparameters! on model
+    lpm = MathProgBase.LinearQuadraticModel(GLPKSolverLP())
+    MathProgBase.setparameters!(lpm, TimeLimit=23.0)
+    @test lpm.param.tm_lim == 23000.0
+
+    # setparameters! on solver
+    lps = GLPKSolverLP()
+    MathProgBase.setparameters!(lps, TimeLimit=23.0)
+    lpm2 = MathProgBase.LinearQuadraticModel(lps)
+    @test lpm2.param.tm_lim == 23000.0
+end
